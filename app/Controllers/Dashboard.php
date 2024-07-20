ee08306790affb9d29c08cf0a647819411811255
vafs92 <126405181+vafs92@users.noreply.github.com>
2024-07-20 22:06:36 +0700

    Update Dashboard.php

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -6,22 +6,30 @@ use App\Models\LokasiWisataModel;
 use App\Models\PendudukModel;
 class dashboard extends BaseController
 {
+    // public function index()
+    // {
+    //     return view('home');
+    // }
+    //  public function penduduk()
+    // {
+    //     $model = new PendudukModel();
+    //     $data['penduduk'] = $model->findAll();
+
+    //     return view('home', $data);
+    // }
+    // public function wisata()
+    // {
+    //     $model = new LokasiWisataModel();
+    //     $data['wisata'] = $model->findAll();
+
+    //     return view('home', $data);
+    // }
     public function index()
     {
-        return view('home');
-    }
-     public function penduduk()
-    {
-        $model = new PendudukModel();
-        $data['penduduk'] = $model->findAll();
-
-        return view('home', $data);
-    }
-    public function wisata()
-    {
-        $model = new LokasiWisataModel();
-        $data['wisata'] = $model->findAll();
-
+        $modelPend = new PendudukModel();
+        $modelWisata = new LokasiWisataModel();
+        $data['penduduk'] = $modelPend->findAll();
+        $data['wisata'] = $modelWisata->findAll();
         return view('home', $data);
     }
 }
